31785ba6933b912b556a430eab909c76332596d2
CrCardd <cris.rc377@gmail.com>
2024-08-09 07:04:55 -0300

    failed

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('botao').addEventListener('click', function() {
+    document.getElementById('button_arrow').addEventListener('click', function() {
         console.log('Button clicked');
         var hiddenDiv = document.getElementById('informations');
         hiddenDiv.style.display = 'block';
     });
 });
+
+
+
+
+
+
+// document.getElementById('button_arrow').addEventListener('click', function() {
+//     document.getElementById('informations').scrollIntoView({ behavior: 'smooth' });
+// }); 
