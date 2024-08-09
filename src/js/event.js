c711154e51371707e4f816f377b57fdca266d407
CrCardd <cris.rc377@gmail.com>
2024-08-09 11:49:55 -0300

    on the way

--- src/js/event.js
+++ src/js/event.js
@@ -2,15 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('button_arrow').addEventListener('click', function() {
         console.log('Button clicked');
         var hiddenDiv = document.getElementById('informations');
-        hiddenDiv.style.display = 'block';
+        hiddenDiv.style.display = 'flex';
+
+        window.scrollBy({
+            top: 700, // Scroll down by the height of the viewport
+            left: 0,
+            behavior: 'smooth'
+        });
     });
 });
 
+// let informations = document.getElementById('informations');
+// let button = document.getElementById('button_arrow');
 
 
-
-
-
-// document.getElementById('button_arrow').addEventListener('click', function() {
-//     document.getElementById('informations').scrollIntoView({ behavior: 'smooth' });
-// }); 
+// button.addEventListener('click', (e) => {
+//     informations.style.display = block
+// });
